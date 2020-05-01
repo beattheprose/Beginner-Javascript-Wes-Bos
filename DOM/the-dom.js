dbccc1c711dbf9e563f11e41b9ac8cffbec81a39
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-01 15:36:27 -0500

    24

--- DOM/the-dom.js
+++ DOM/the-dom.js
@@ -1,6 +1,10 @@
 const pic = document.querySelector(`.nice`);
-console.log(pic.classList);
 
 const toggleRound = () => pic.classList.toggle(`round`);
 
 pic.addEventListener(`click`, toggleRound);
+
+pic.alt = `photographer`;
+
+console.log(pic.naturalWidth);
+
