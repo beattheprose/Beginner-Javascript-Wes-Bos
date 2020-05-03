3a3834078d581f13c82429325ac5997c64d550fc
Alex Jackson <45179933+beattheprose@users.noreply.github.com>
2020-05-03 16:09:18 -0500

    31 and 32

---
+++ events/forms.js
@@ -0,0 +1,26 @@
+const wes = document.querySelector(`.wes`);
+
+wes.addEventListener(`click`, e => {
+  console.log(`Clicked!`);
+  const shouldChange = confirm(`This Website will change?`);
+  if (!shouldChange) {
+    e.preventDefault();
+  }
+  console.log(shouldChange);
+});
+
+const signupForm = document.querySelector(`[name='signup']`);
+signupForm.addEventListener(`submit`, e => {
+  const name = e.currentTarget.name.value;
+  if (name.includes(`chad`)) {
+    alert(`get out of my swamp`);
+    e.preventDefault();
+  }
+});
+
+const logEvent = e => {
+  console.log(e.type);
+  console.log(e.currentTarget.value);
+};
+signupForm.name.addEventListener(`keyup`, logEvent);
+signupForm.name.addEventListener(`keydown`, logEvent);
